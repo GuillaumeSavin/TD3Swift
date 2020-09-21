c74200ece18dec631ea146b3d88b6d55c38e8394
GuillaumeSavin <guisav1996@hotmail.fr>
2020-09-21 10:28:33 +0200

    Initial Commit

---
+++ TD3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TD3
+//
+//  Created by Guillaume Savin on 21/09/2020.
+//  Copyright © 2020 Guillaume Savin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
